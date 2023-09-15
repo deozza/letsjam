6e0999f0ba323a29eca362bdf7a27e1bc7b73741
Edenn Touitou <deozza@gmail.com>
2023-09-15 01:36:04 +0000

    updated version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,65 @@
 <script lang="ts">
-	import { TanoshiLinkModel, TanoshiNavigation, TanoshiNavigationModel } from 'tanoshi';
+	import { SIZES, THEMES, TanoshiLink, TanoshiLinkModel, TanoshiNavigation, TanoshiNavigationModel } from 'tanoshi';
     import './app.css';
+	import type { TanoshiNavigationLinkModel } from 'tanoshi/dist/types/Types';
 
-    const devsLink: TanoshiLinkModel = new TanoshiLinkModel('Développeur web').setLink('/website-creation').setTheme('primary').setDisplaySize('lg');
-    const coursesLink: TanoshiLinkModel = new TanoshiLinkModel('Formations').setLink('/courses').setTheme('primary').setDisplaySize('lg');
-    const logoLink: TanoshiLinkModel = new TanoshiLinkModel('LETSJAM').setLink('/').setTheme('primary').setDisplaySize('2xl');
-    const resumeLink: TanoshiLinkModel = new TanoshiLinkModel('CV').setLink('/resume').setTheme('primary').setDisplaySize('lg');
-    const contactLink: TanoshiLinkModel = new TanoshiLinkModel('Me contacter').setLink('/contact').setTheme('primary').setDisplaySize('lg');
+    const devsLink: TanoshiLinkModel = new TanoshiLinkModel('Développeur web')
+        .setLink('/website-creation')
+        .setTheme(THEMES.Primary)
+        .setDisplaySize(SIZES.Lg);
+    const coursesLink: TanoshiLinkModel = new TanoshiLinkModel('Formations')
+        .setLink('/courses')
+        .setTheme(THEMES.Primary)
+        .setDisplaySize(SIZES.Lg);
+    const logoLink: TanoshiLinkModel = new TanoshiLinkModel('LETSJAM')
+        .setLink('/')
+        .setTheme(THEMES.Primary)
+        .setDisplaySize(SIZES['2Xl']);
+    const resumeLink: TanoshiLinkModel = new TanoshiLinkModel('CV')
+        .setLink('/resume')
+        .setTheme(THEMES.Primary)
+        .setDisplaySize(SIZES.Lg);
+    const contactLink: TanoshiLinkModel = new TanoshiLinkModel('Me contacter')
+        .setLink('/contact')
+        .setTheme(THEMES.Primary)
+        .setDisplaySize(SIZES.Lg);
    
-    const navigationLinksCenter: Array<TanoshiLinkModel> = [
-        devsLink,
-        logoLink,
-        coursesLink
-    ];
+    const navbarCenterItemModels: Array<TanoshiNavigationLinkModel> = [
+		{
+			link: devsLink,
+			component: TanoshiLink
+		},
+		{
+			link: logoLink,
+			component: TanoshiLink
+		},
+		{
+			link: coursesLink,
+			component: TanoshiLink
+		},
+	];
 
-    const navigationLinksRight: Array<TanoshiLinkModel> = [
-        resumeLink,
-        contactLink
-    ];
+    const navbarRightItemModels: Array<TanoshiNavigationLinkModel> = [
+		{
+			link: resumeLink,
+			component: TanoshiLink
+		},
+		{
+			link: contactLink,
+			component: TanoshiLink
+		}
+	];
+
 
     const tanoshiNavigationModel: TanoshiNavigationModel = new TanoshiNavigationModel()
-    .setTheme('dark')
-    .setItemsAtCenter(navigationLinksCenter)
-    .setItemsAtRight(navigationLinksRight);
+        .setTheme(THEMES.Black)
+        .setItemsAtCenter(navbarCenterItemModels)
+        .setItemsAtRight(navbarRightItemModels);
+
 </script>
 
 <div>
-    <TanoshiNavigation {tanoshiNavigationModel} />
+    <TanoshiNavigation tanoshiDesktopNavigationModel={tanoshiNavigationModel}  tanoshiMobileNavigationModel={tanoshiNavigationModel} />
     <main class="relative">
         <slot />
     </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,117 +1,134 @@
 <script lang="ts">
-	import { TanoshiButtonModel, TanoshiContainer, TanoshiContainerModel, TanoshiHeader, TanoshiHeaderModel, TanoshiLink, TanoshiLinkModel, TanoshiParagraph, TanoshiParagraphModel } from "tanoshi";
+	import { 
+        CONTAINER_ITEMS_ALIGNMENTS, 
+        CONTAINER_ITEMS_SPACING, 
+        CONTAINER_ORIENTATIONS, 
+        HEADER_TAGS, 
+        HEIGHTS, 
+        SIZES, 
+        TEXT_ALIGNMENT, 
+        THEMES, 
+        WIDTHS,
+        TanoshiButtonModel, 
+        TanoshiContainer, 
+        TanoshiContainerModel, 
+        TanoshiHeader, 
+        TanoshiHeaderModel,
+        TanoshiLinkModel, 
+        TanoshiParagraph, 
+        TanoshiParagraphModel
+        } from "tanoshi";
+	import TanoshiLinkAsButtonModel from "tanoshi/dist/molecules/link/TanoshiLinkAsButton/TanoshiLinkAsButtonModel";
 
-    const heroBackgroundContainer: TanoshiContainerModel = new TanoshiContainerModel('c')
-        .setTheme('primary')
-        .setDesktopSpacing('centered')
-        .setHeight('min-h-100-vh')
+    const heroBackgroundContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.C)
+        .setBackgroundTheme(THEMES.Primary)
+        .setDesktopSpacing(CONTAINER_ITEMS_SPACING.Centered)
+        .setHeight(HEIGHTS.MINH100VH)
 
-    const heroContentContainer: TanoshiContainerModel = new TanoshiContainerModel('r')
-        .setDesktopSpacing('around')
+    const heroContentContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.R)
+        .setDesktopSpacing(CONTAINER_ITEMS_SPACING.Around)
 
-    const heroLeftContentContainer: TanoshiContainerModel = new TanoshiContainerModel('c')
-        .setDesktopSpacing('centered')
-        .setItemsAlignment('center')
-        .setSize('w-10')
+    const heroLeftContentContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.C)
+        .setDesktopSpacing(CONTAINER_ITEMS_SPACING.Centered)
+        .setItemsAlignment(CONTAINER_ITEMS_ALIGNMENTS.Center)
+        .setWidth(WIDTHS.W10)
 
     const heroHeaderModel: TanoshiHeaderModel = new TanoshiHeaderModel('Composons ensemble votre site web')
-        .setDisplaySize('6xl')
-        .setHtmlSize('h1')
-        .setAligment('center');
+        .setSize(SIZES["6Xl"])
+        .setTag(HEADER_TAGS.H1)
+        .setAligment(TEXT_ALIGNMENT.Center);
 
     const heroParagraphModel : TanoshiParagraphModel = new TanoshiParagraphModel("AUDIT, CONCEPTION, DÉVELOPPEMENT, HÉBERGEMENT, MAINTENANCE")
-        .setDisplaySize('3xl')
-        .setAlignment('center')
+        .setSize(SIZES["3Xl"])
+        .setAlignment(TEXT_ALIGNMENT.Center)
         .setHasSpacing(false);
 
     const heroParagraphBisModel : TanoshiParagraphModel = new TanoshiParagraphModel("POUR LES ARTISANS DE LA MUSIQUE")
-        .setDisplaySize('3xl')
-        .setAlignment('center')
+        .setSize(SIZES["3Xl"])
+        .setAlignment(TEXT_ALIGNMENT.Center)
         .setHasSpacing(false);
 
-    const aboutBackgroundContainer: TanoshiContainerModel = new TanoshiContainerModel('r')
-        .setHeight('min-h-100-vh')
-        .setDesktopSpacing('around')
+    const aboutBackgroundContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.R)
+        .setHeight(HEIGHTS.MINH100VH)
+        .setDesktopSpacing(CONTAINER_ITEMS_SPACING.Around)
 
-    const aboutContentHalfLeftContainer: TanoshiContainerModel = new TanoshiContainerModel('c')
-        .setDesktopSpacing('centered')
-        .setItemsAlignment('start')
-        .setSize('w-6')
-        .setHeight('min-h-100-vh')
+    const aboutContentHalfLeftContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.C)
+        .setDesktopSpacing(CONTAINER_ITEMS_SPACING.Centered)
+        .setItemsAlignment(CONTAINER_ITEMS_ALIGNMENTS.Start)
+        .setWidth(WIDTHS.W6)
+        .setHeight(HEIGHTS.MINH100VH)
 
-    const aboutContentHalfRightContainer: TanoshiContainerModel = new TanoshiContainerModel('c')
-        .setDesktopSpacing('centered')
-        .setSize('w-6')
-        .setHeight('min-h-100-vh')
-        .setItemsAlignment('center')
+    const aboutContentHalfRightContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.C)
+        .setDesktopSpacing(CONTAINER_ITEMS_SPACING.Centered)
+        .setWidth(WIDTHS.W6)
+        .setHeight(HEIGHTS.MINH100VH)
+        .setItemsAlignment(CONTAINER_ITEMS_ALIGNMENTS.Center)
 
     const aboutHeaderModel: TanoshiHeaderModel = new TanoshiHeaderModel('A propos de moi')
-        .setDisplaySize('6xl')
-        .setHtmlSize('h2')
-        .setAligment('center')
-        .setTheme('primary');
+        .setSize(SIZES["6Xl"])
+        .setTag(HEADER_TAGS.H2)
+        .setAligment(TEXT_ALIGNMENT.Center)
+        .setTheme(THEMES.Primary);
 
-    const emptyBackgroundContainer: TanoshiContainerModel = new TanoshiContainerModel('r')
-        .setDesktopSpacing('centered')
+    const emptyBackgroundContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.R)
+        .setDesktopSpacing(CONTAINER_ITEMS_SPACING.Centered)
 
     const servicesHeaderModel: TanoshiHeaderModel = new TanoshiHeaderModel('Mes services')
-        .setDisplaySize('6xl')
-        .setHtmlSize('h2')
-        .setAligment('center')
-        .setTheme('black');
+        .setSize(SIZES["6Xl"])
+        .setTag(HEADER_TAGS.H2)
+        .setAligment(TEXT_ALIGNMENT.Center)
+        .setTheme(THEMES.Black);
 
-    const servicesContentContainer: TanoshiContainerModel = new TanoshiContainerModel('r')
-        .setDesktopSpacing('around')
+    const servicesContentContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.R)
+        .setDesktopSpacing(CONTAINER_ITEMS_SPACING.Around)
     
-    const servicesContentLeftContainer: TanoshiContainerModel = new TanoshiContainerModel('c')
-        .setTheme('primary')
-        .setSize('w-6')
-        .setHeight('min-h-100-vh')
-        .setItemsAlignment('center')
+    const servicesContentLeftContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.C)
+        .setBackgroundTheme(THEMES.Primary)
+        .setWidth(WIDTHS.W6)
+        .setHeight(HEIGHTS.MINH100VH)
+        .setItemsAlignment(CONTAINER_ITEMS_ALIGNMENTS.Center)
 
     const servicesDevelopmentHeader: TanoshiHeaderModel = new TanoshiHeaderModel('Développement')
-        .setDisplaySize('4xl')
-        .setHtmlSize('h3')
-        .setAligment('center')
-        .setTheme('black');
+        .setSize(SIZES["4Xl"])
+        .setTag(HEADER_TAGS.H3)
+        .setAligment(TEXT_ALIGNMENT.Center)
+        .setTheme(THEMES.Black);
 
-    const servicesContentRightContainer: TanoshiContainerModel = new TanoshiContainerModel('c')
-        .setTheme('dark')
-        .setSize('w-6')
-        .setHeight('min-h-100-vh')
-        .setItemsAlignment('center')
+    const servicesContentRightContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.C)
+        .setBackgroundTheme(THEMES.Black)
+        .setWidth(WIDTHS.W6)
+        .setHeight(HEIGHTS.MINH100VH)
+        .setItemsAlignment(CONTAINER_ITEMS_ALIGNMENTS.Center)
 
     const devKnowMoreButton: TanoshiButtonModel = new TanoshiButtonModel('En savoir plus')
-        .setTheme('dark')
-
+        .setBasicTheme(THEMES.Black)
     const devKnowMoreLink: TanoshiLinkModel = new TanoshiLinkModel('Formations')
-        .setContentAsButton(devKnowMoreButton)
         .setLink('/website-creation')
+    const devKnowMoreLinkAsButton: TanoshiLinkAsButtonModel = new TanoshiLinkAsButtonModel(devKnowMoreLink, devKnowMoreButton)
 
     const servicesFormationsHeader: TanoshiHeaderModel = new TanoshiHeaderModel('Formations')
-        .setDisplaySize('4xl')
-        .setHtmlSize('h3')
-        .setAligment('center')
-        .setTheme('primary');
+        .setSize(SIZES["4Xl"])
+        .setTag(HEADER_TAGS.H3)
+        .setAligment(TEXT_ALIGNMENT.Center)
+        .setTheme(THEMES.Primary);
 
     const formationKnowMoreButton: TanoshiButtonModel = new TanoshiButtonModel('En savoir plus')
-        .setTheme('primary')
-
+        .setBasicTheme(THEMES.Primary)
     const formationKnowMoreLink: TanoshiLinkModel = new TanoshiLinkModel('Formations')
-        .setContentAsButton(formationKnowMoreButton)
         .setLink('/courses')
+    const formationKnowMoreLinkAsButton: TanoshiLinkAsButtonModel = new TanoshiLinkAsButtonModel(formationKnowMoreLink, formationKnowMoreButton)
 
-    const contactBackgroundContainer: TanoshiContainerModel = new TanoshiContainerModel('c')
-        .setTheme('primary')
-        .setDesktopSpacing('centered')
+    const contactBackgroundContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.C)
+        .setBackgroundTheme(THEMES.Primary)
+        .setDesktopSpacing(CONTAINER_ITEMS_SPACING.Centered)
 
-    const contactContentContainer: TanoshiContainerModel = new TanoshiContainerModel('r')
-        .setDesktopSpacing('around')
+    const contactContentContainer: TanoshiContainerModel = new TanoshiContainerModel(CONTAINER_ORIENTATIONS.R)
+        .setDesktopSpacing(CONTAINER_ITEMS_SPACING.Around)
     
     const contactHeaderModel: TanoshiHeaderModel = new TanoshiHeaderModel('Une question ? Contactez-moi !')
-        .setDisplaySize('6xl')
-        .setHtmlSize('h2')
-        .setAligment('center');
+        .setSize(SIZES["6Xl"])
+        .setTag(HEADER_TAGS.H2)
+        .setAligment(TEXT_ALIGNMENT.Center);
 
 </script>
 
@@ -151,12 +168,10 @@
     <TanoshiContainer tanoshiContainerModel={servicesContentContainer} customClasses={'no-padding'}>
         <TanoshiContainer tanoshiContainerModel={servicesContentLeftContainer}>
             <TanoshiHeader tanoshiHeaderModel={servicesDevelopmentHeader} />
-            <TanoshiLink tanoshiLinkModel={devKnowMoreLink} />
 
         </TanoshiContainer>
         <TanoshiContainer tanoshiContainerModel={servicesContentRightContainer}>
             <TanoshiHeader tanoshiHeaderModel={servicesFormationsHeader} />
-            <TanoshiLink tanoshiLinkModel={formationKnowMoreLink} />
         </TanoshiContainer>
     </TanoshiContainer>
 </section>
