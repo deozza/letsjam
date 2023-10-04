be8cf72528021d18fec6a79c6307351212317ebc
Edenn Touitou <deozza@gmail.com>
2023-10-04 13:15:57 +0000

    adapted contact to firestore trigger mail ext

--- src/routes/api/contact/+server.ts
+++ src/routes/api/contact/+server.ts
@@ -4,11 +4,20 @@ import { setDoc, doc } from "firebase/firestore";
 export async function POST({ request}) {
 	const data = await request.formData();
 	const formJSON = Object.fromEntries(data.entries());
-	formJSON.timestamp = Date.now();
+	const payload = {
+		to: import.meta.env.VITE_CONTACT_EMAIL,
+		from: formJSON.name + ' ' + formJSON.email,
+		message: {
+			subject: formJSON.informationTypeChoice,
+			text: formJSON.content,
+			html: formJSON.content
+		},
+		timestamp: Date.now()
+	}
 
 	let status = 201
 
-	await setDoc(doc(db, "contacts", formJSON.timestamp.toString()), formJSON)
+	await setDoc(doc(db, "contacts", payload.timestamp.toString()), payload)
 		.catch(() => { 
 			status = 400
 		})
